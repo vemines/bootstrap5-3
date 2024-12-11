b6c06d6ac51fd279bc5f574c1bede612288c7cc8
VeMines <vemines@outlook.com>
2024-12-12 03:08:37 +0700

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Bootstrap 5.3
+
+## What is Bootstrap
+
+Bootstrap 5 is widely used in front-end development to create responsive, mobile-first websites. It offers a rich set of pre-built components and a powerful grid system, making layout design straightforward. By integrating Bootstrap, developers can quickly build and deploy a polished, responsive UI with minimal custom CSS or JavaScript. It’s perfect for creating consistent, accessible user interfaces with less effort.
+
+## Overview
+
+This project is designed for learning purposes, making it easier to understand and apply Bootstrap for building responsive, modern web interfaces. Based on the official [Bootstrap documentation](https://getbootstrap.com/docs/5.3/getting-started/introduction/). It showcases a collection of components with practical examples for hands-on experimentation.
+
+**Note:** aria attributes, roles, and other non-UI elements have been removed simplify the code for learning. However, it is recommended to understand and implement accessibility features in production projects. For full examples, refer to the official documentation.
+
+[Demo link](https://vemines.github.io/bootstrap5-3)
+
+## Getting Started
+
+1. **Clone the Repository**:
+
+   ```bash
+   git clone <repository-url>
+   cd <repository-directory>
+   ```
+
+2. **Open the `index.html` File**:
+   Open the `index.html` file in your preferred web browser to view the playground.
+
+3. **Experiment**:
+   Feel free to modify the HTML and CSS to see how changes affect the layout and functionality of the components.
+
+## License
+
+This project is open-source and available under the [MIT License](LICENSE).
+
+## Acknowledgments
+
+- [Bootstrap](https://getbootstrap.com/) - description.
+
+- Created by VeMines with love ❤️. If you like this project please star ⭐ it
---
+++ js/main.js
@@ -0,0 +1,122 @@
+const tabs = [
+  'accordion-collapse',
+  'alert',
+  'badge',
+  'border-shadow',
+  'breadcrumb',
+  'breakpoint',
+  'button',
+  'card',
+  'carousel',
+  'color',
+  'container',
+  'display-position',
+  'dropdown',
+  'flexbox',
+  'forms',
+  'grid-column',
+  'image',
+  'list-group',
+  'modal',
+  'nav-tab',
+  'navbar',
+  'offcanvas',
+  'pagination',
+  'placeholder',
+  'popover-tooltip',
+  'progress-bar',
+  'scrollspy',
+  'size-spacing',
+  'spinner',
+  'table',
+  'toast',
+  'typography',
+  'utilities',
+];
+
+(() => {
+  const template = document.getElementById('tab-template');
+  if (!template) return;
+
+  const tabListElement = document.getElementById('pills-tab');
+  if (!tabListElement) return;
+
+  for (let tab of tabs) {
+    const tabElement = template.content.firstElementChild.cloneNode(true);
+    const button = tabElement.querySelector('.nav-link');
+    button.id = `nav-${tab}`;
+    button.textContent = tab
+      .split('-')
+      .map((t) => t.toUpperCase())
+      .join(' & ');
+    button.setAttribute('data-bs-target', `#${tab}`);
+
+    tabListElement.appendChild(tabElement);
+  }
+  tabListElement.firstChild.querySelector('.nav-link').classList.add('active');
+})();
+
+// Validate form
+const form = document.querySelector('.form-validation');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (!form.checkValidity()) {
+    form.classList.add('was-validated');
+  } else {
+    const formData = new FormData(form);
+
+    // formData.forEach((value, key) => {
+    //   console.log(key, value);
+    // });
+
+    // form.submit();
+  }
+});
+
+// Initialize tooltips and popovers
+document.addEventListener('DOMContentLoaded', function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+});
+
+// Toast
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+const toast1 = document.querySelector('#toast1');
+const toast2 = document.querySelector('#toast2');
+const toast3 = document.querySelector('#toast3');
+
+const btnStack = document.querySelector('#btnStack');
+const toastStack = document.querySelector('.toastStack');
+
+const toast1Bootstrap = bootstrap.Toast.getOrCreateInstance(toast1);
+btn1.addEventListener('click', () => toast1Bootstrap.show());
+const toast2Bootstrap = bootstrap.Toast.getOrCreateInstance(toast2);
+btn2.addEventListener('click', () => toast2Bootstrap.show());
+const toast3Bootstrap = bootstrap.Toast.getOrCreateInstance(toast3);
+btn3.addEventListener('click', () => toast3Bootstrap.show());
+
+btnStack.addEventListener('click', function () {
+  const toastElements = toastStack.querySelectorAll('.toast');
+  toastElements.forEach((toastEl) => {
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+  });
+});
+
+// Scroll to top
+let scrollTop = document.getElementById('scrollTop');
+
+// Scroll to top when clicked
+scrollTop.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
